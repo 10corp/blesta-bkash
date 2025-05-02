ca815ae30f86ab02d23ff61e5826ea90d18099d9
Mahmudul Hasan pasha <11585101+10corp@users.noreply.github.com>
2025-05-02 14:01:48 +0000

    Initial commit

--- language/en_us/bkash.php
+++ language/en_us/bkash.php
@@ -1,21 +1,25 @@
 <?php
-$lang['Bkash.name'] = 'bKash';
-$lang['Bkash.username'] = 'Username';
-$lang['Bkash.password'] = 'Password';
-$lang['Bkash.app_key'] = 'App Key';
-$lang['Bkash.app_secret'] = 'App Secret';
-$lang['Bkash.sandbox'] = 'Sandbox Mode';
-$lang['Bkash.redirect_button'] = 'Proceed to bKash';
-$lang['Bkash.!error.username.empty'] = 'Please enter a username.';
-$lang['Bkash.!error.password.empty'] = 'Please enter a password.';
-$lang['Bkash.!error.app_key.empty'] = 'Please enter an app key.';
-$lang['Bkash.!error.app_secret.empty'] = 'Please enter an app secret.';
-$lang['Bkash.!error.sandbox.valid'] = 'Sandbox mode must be "true" or "false".';
-$lang['Bkash.!error.configuration.missing'] = 'Gateway configuration is incomplete.';
-$lang['Bkash.!error.token.failed'] = 'Failed to obtain bKash token.';
-$lang['Bkash.!error.payment.failed'] = 'Payment creation failed.';
-$lang['Bkash.!error.request.failed'] = 'API request failed.';
-$lang['Bkash.!error.callback.invalid'] = 'Invalid callback received.';
-$lang['Bkash.!error.transaction.failed'] = 'Transaction not completed.';
-$lang['Bkash.!error.client.missing'] = 'Client ID is required.';
+// Error messages
+$lang['Bkash.!error.gateway_not_installed'] = 'The bKash gateway is not installed.';
+$lang['Bkash.!error.payment_failed'] = 'The payment could not be processed. Please try again.';
+$lang['Bkash.!error.username.valid'] = 'Please enter a valid Username.';
+$lang['Bkash.!error.password.valid'] = 'Please enter a valid Password.';
+$lang['Bkash.!error.app_key.valid'] = 'Please enter a valid App Key.';
+$lang['Bkash.!error.app_secret.valid'] = 'Please enter a valid App Secret.';
+
+// Success messages
+$lang['Bkash.!success.payment_processed'] = 'Your payment was successfully processed.';
+
+// Process page
+$lang['Bkash.process.title'] = 'Pay with bKash';
+$lang['Bkash.process.heading'] = 'Complete Your Payment';
+$lang['Bkash.process.description'] = 'You will be redirected to bKash to complete your payment. Please follow the instructions there to finalize the transaction.';
+
+// Settings page
+$lang['Bkash.settings.heading'] = 'bKash Gateway Settings';
+$lang['Bkash.settings.username'] = 'Username';
+$lang['Bkash.settings.password'] = 'Password';
+$lang['Bkash.settings.app_key'] = 'App Key';
+$lang['Bkash.settings.app_secret'] = 'App Secret';
+$lang['Bkash.settings.submit'] = 'Save Settings';
 ?>
